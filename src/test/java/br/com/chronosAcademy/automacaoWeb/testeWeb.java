7fd4c7e37f34ba7a8fae70fef565d013244423b4
kleberaalmeida <42724722+kleberaalmeida@users.noreply.github.com>
2024-06-21 16:07:22 -0300

    aula 06 anotacoes

--- src/test/java/br/com/chronosAcademy/automacaoWeb/testeWeb.java
+++ src/test/java/br/com/chronosAcademy/automacaoWeb/testeWeb.java
@@ -1,6 +1,8 @@
 package br.com.chronosAcademy.automacaoWeb;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
@@ -12,37 +14,36 @@ import static org.junit.Assert.assertEquals;
 
 public class testeWeb {
 
-    @Test
-    public void primeiroTeste() {
+    WebDriver driver;
+
+    @Before
+    public void inicializaTeste(){
         WebDriverManager.edgedriver().setup();
-        WebDriver driver;
         driver = new EdgeDriver();
         driver.manage().window().maximize();
-
         driver.get("https://www.chronosacademy.com.br");
+    }
+
+    @Test
+    public void primeiroTeste() {
         String xpathTitulo = "/html/body/div/div/div/section[2]/div[3]/div/div/div[1]/div/h4";
         WebElement txtTitulo = driver.findElement(By.xpath(xpathTitulo));
         String titulo = txtTitulo.getText();
         assertEquals("Porque Tempo É Conhecimento.", titulo);
-
-        driver.quit();
     }
 
     @Test
     public void segundoTeste() {
-        WebDriverManager.edgedriver().setup();
-        WebDriver driver;
-        driver = new EdgeDriver();
-        driver.manage().window().maximize();
-
-        driver.get("https://www.chronosacademy.com.br");
         String xpathTitulo = "/html/body/div/div/div/section[2]/div[3]/div/div/div[2]/div/div/a/span/span[2]";
         driver.findElement(By.xpath(xpathTitulo)).click();
         String xpathSegundoTitulo = "//*[@id=\"block-214\"]/div/div/div/div[1]/div/div";
         WebElement txtSegundoTitulo = driver.findElement(By.xpath(xpathSegundoTitulo));
         String titulo = txtSegundoTitulo.getText();
         assertEquals("Conheça todos os nossos cursos.", titulo);
+    }
 
+    @After
+    public void finalizaTeste(){
         driver.quit();
     }
 }
