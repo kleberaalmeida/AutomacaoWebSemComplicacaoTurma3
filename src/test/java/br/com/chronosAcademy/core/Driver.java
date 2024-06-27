ce6ec7cd3c64a6564588857fcd88a5fece41371e
kleberaalmeida <42724722+kleberaalmeida@users.noreply.github.com>
2024-06-27 16:06:19 -0300

    aula 08 anotacoes

---
+++ src/test/java/br/com/chronosAcademy/core/Driver.java
@@ -0,0 +1,46 @@
+package br.com.chronosAcademy.core;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+
+public class Driver {
+
+    private WebDriver driver;
+
+    public Driver (String navegador) {
+        switch (navegador) {
+            case "edge":
+                WebDriverManager.edgedriver().setup();
+                driver = new EdgeDriver();
+                break;
+                case "chrome":
+                    WebDriverManager.chromedriver().setup();
+                    driver = new ChromeDriver();
+                    break;
+                    case "ie":
+                        WebDriverManager.iedriver().setup();
+                        driver = new InternetExplorerDriver();
+                        break;
+                        case "firefox":
+                            WebDriverManager.firefoxdriver().setup();
+                            driver = new FirefoxDriver();
+                            break;
+                        default:
+                                WebDriverManager.edgedriver().setup();
+                                driver = new EdgeDriver();
+                                break;
+                        }
+                        driver.manage().window().maximize();
+                    }
+                    public WebDriver getDriver() {
+                        return driver;
+    }
+}
+
+
+
+
